85cd2a0ab0334a0f2ff443ef67d18ceff96cdcaf
Harsh Chaturvedi <harsh@Mac-68.lan>
2026-07-16 01:46:03 +0530

    basic spring project is running

---
+++ Part05-What Is Spring Boot/src/main/java/com/telusko/DemoApp/Hello.java
@@ -0,0 +1,14 @@
+package com.telusko.DemoApp;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Hello {
+
+    @RequestMapping("/")
+    public String greet(){
+        System.out.println("greet() called");
+        return "Hello world";
+    }
+}
